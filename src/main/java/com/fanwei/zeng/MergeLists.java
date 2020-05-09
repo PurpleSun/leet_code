d8f13b86a38002a2ac1ce0ac91519254bf92ddd9
zengfanwei <zengfanwei@qutoutiao.net>
2020-05-09 19:02:49 +0800

    21. Merge Two Sorted Lists

---
+++ src/main/java/com/fanwei/zeng/MergeLists.java
@@ -0,0 +1,44 @@
+package com.fanwei.zeng;
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+public class MergeLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode left = l1;
+        ListNode right = l2;
+        ListNode newHead = null;
+        ListNode pointer = null;
+        while (left != null && right != null) {
+            ListNode ListNode;
+            if (left.val < right.val) {
+                ListNode = new ListNode(left.val);
+                left = left.next;
+            } else {
+                ListNode = new ListNode(right.val);
+                right = right.next;
+            }
+            if (newHead == null) {
+                newHead = ListNode;
+                pointer = ListNode;
+            } else {
+                pointer.next = ListNode;
+                pointer = pointer.next;
+            }
+        }
+
+        for (ListNode node : new ListNode[]{left, right}) {
+            while (node != null) {
+                ListNode ListNode = new ListNode(node.val);
+                if (newHead == null) {
+                    newHead = ListNode;
+                    pointer = ListNode;
+                } else {
+                    pointer.next = ListNode;
+                    pointer = pointer.next;
+                }
+                node = node.next;
+            }
+        }
+
+        return newHead;
+    }
+}
